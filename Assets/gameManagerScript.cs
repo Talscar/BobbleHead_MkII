329c2c0741f315a93e098b1ea2815ffc7d259a4c
Talscar-PC\Talscar <talscartalcom@gmail.com>
2017-12-07 21:34:38 +1030

    #Game manager works and time till start functions correctly!
    #Bug with button resolved not reappearing when it dissapeared and the match ended.

--- Assets/gameManagerScript.cs
+++ Assets/gameManagerScript.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class gameManagerScript : MonoBehaviour {
 
-    public float time_ToComplete = 60f;
-    public float time_TillStart = 5f; //Disable shaving tool till READY!
-    public float time;
-    float time_TimetimeStart = 0;
 
-	// Use this for initialization
+    /// <summary>
+    /// How many seconds we want the match to run.
+    /// </summary>
+    public float time_ToComplete = 60f;
+    
+    /// <summary>
+    /// The time in which is a delay before starting
+    /// </summary>
+    public float time_TillStart = 4f; //Disable shaving tool till READY!
+    
+    /// <summary>
+    /// Current time in the game.
+    /// </summary>
+    //public float time;
+
+    /// <param name="time_TimetimeStart">When the match started!<param name="time_TimetimeStart"></param></param>
+    float time_TimetimeStart = 0; //When the game started
+
 	void Start () {
         player = FindObjectOfType<PlayerScreenPointToClick>();
         player.DestroyBeard = false;
@@ -17,11 +30,14 @@ public class gameManagerScript : MonoBehaviour {
         {
             button.onClick.AddListener(gameStart);
         }
+        //time += time_ToComplete + time_TillStart;
 	}
 
 
     PlayerScreenPointToClick player;
     public UnityEngine.UI.Button button;
+    public UnityEngine.UI.Text CountDown;
+
     //bool canInteract;
     /// <summary>
     /// When the game starts, it processes a timer and calculates when the game start and initilizes player ability to interact with world objects or not.
@@ -29,14 +45,19 @@ public class gameManagerScript : MonoBehaviour {
     void gameStart()
     {
         time_TimetimeStart = Time.time;
-        player.DestroyBeard = true;
+        //time_TimetimeStart = time;
+        //player.DestroyBeard = true;
         button.gameObject.SetActive(false);
+        running = true;
     }
+
+    bool running = false;
     /// <summary>
     /// When the game ends, it processes a timer and calculates the end game time to dissable the players ability to interact with the world objects.
     /// </summary>
     void gameEnd()
     {
+        running = false;
         player.DestroyBeard = false;
         if(button != null)
         {
@@ -45,14 +66,25 @@ public class gameManagerScript : MonoBehaviour {
     }
 	// Update is called once per frame
 	void Update () {
-
-        if(time < time_ToComplete + time_TillStart)
+        if (running)
         {
-            time = Time.time - time_TimetimeStart;
-        }
-        else
-        {
-            gameEnd();
+            if (Time.time - (time_TillStart) < time_TimetimeStart)
+            {
+                float counter = time_TimetimeStart - (Time.time -  time_TillStart);
+                if (CountDown != null && counter > -0.1)
+                {
+                    CountDown.text = "" + (int)counter + "...";
+                }
+            }
+            else if (Time.time - (time_ToComplete + time_TillStart) < time_TimetimeStart)
+            {
+                CountDown.text = "";
+                player.DestroyBeard = true;
+            }
+            else
+            {
+                gameEnd();
+            }
         }
 	}
 }
