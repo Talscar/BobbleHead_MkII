df10a32152285b6e97366bf06fcb78fcbec56e4b
AIE\s171*** <talscartalcom@gmail.com>
2017-12-08 14:27:36 +1030

    #Bobble forces #Sensitivity options #Glass like matirial
    ###Removing springs for various control reasons and purposes! to be replaced.
    ###Expected Jam to not be 100% Successful

--- Assets/FaceBobble_Relocator.cs
+++ Assets/FaceBobble_Relocator.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class FaceBobble_Relocator : MonoBehaviour {
     public Transform faceToRelocate;
+    Vector3 ownRefrence;
+
+    [System.Serializable]public struct sensitivityVariables
+    {
+        [Header("Rotation sensitivity and store")]
+        public float maxRange;
+        public float maxRotation;
+        public Vector3 eular_Rotation_Origin;
+        [Header("Sensitivity Options")]
+        public float weighing_Force;
+        public float bobbleReturn_Force;
+        public float constant_Force;
+        
+    }
+    public sensitivityVariables Sensitivity_Options;
 	// Use this for initialization
 	void Start () {
         if(faceToRelocate == null)
         {
+            //vertices = mesh.vertices;
+            ////vertOriginalCoordinates = vertices;
+            //vertOriginalCoordinates = new Vector3[vertices.Length];
+            //for (int i = 0; i < vertices.Length;)
+            //{
+            //    vertOriginalCoordinates[i] = new Vector3(vertices[i].x, vertices[i].y, vertices[i].z);
+
+            //    i++;
+            //}
+            //ownRefrence ;
+            ownRefrence = new Vector3(faceToRelocate.transform.position.x, faceToRelocate.transform.position.y, faceToRelocate.transform.position.z);
             //Transform[] children = transform.GetChild;
             //foreach(Transform child in transform.children)
             Debug.LogError("If the Face transform is not present, the game can not return face to origin location!");
