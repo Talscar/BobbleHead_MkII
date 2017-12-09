3dd4cd194ace3e64bdd02238ac3e38dfd006834b
Talscar-PC\Talscar <talscartalcom@gmail.com>
2017-12-09 15:17:53 +1030

    #liveGameSetup script new functionality working as intended in transformFilter() {} function. - Filters through all child transforms to find stops and numerical points to go too. - Filters the transform order.
    You can:
    - Make a new transform and within the name you should have a number and [stop] as the point your to shave at.
    - Have a child object for shaving at stop points.
    
    You can't:
    - Have numbers in anything other than the points you are to move towards! If in any children or children of children is a number the system could break!
    - Have numbers in the parent. (It's very inclusive of the parent as a child!)
    - Have a number at the begining and middle and the end. It will add them as one number. 13HelloMichael05ThePickle2017 = "13052017".
    - Not have a order of (0, 1, 2, 3, etc) and say (0, 11, 72, 115, 99999, etc)
    
    Testing required:
    - More than 10 or 100 possible number variations.
    - Filter order and numbers correctly rolled out at high numerical values.

--- Assets/liveGameSetup.cs
+++ Assets/liveGameSetup.cs
@@ -79,14 +79,16 @@ public class liveGameSetup : MonoBehaviour {
     {
         Transform[] children = GetComponentsInChildren<Transform>();
         //int highestNumericValue = 0;
-        foreach(Transform child in children)
+
+        //Filter all Transforms for Numerical transforms for highest numerical values.
+        foreach (Transform child in children)
         {
             int nameNum = 0;
             int returning;
             bool returnable;
 
             string newNum = "";
-            Debug.LogError("Require research and assistance converting transform name to int number!");
+            //Debug.LogError("Require research and assistance converting transform name to int number!");
             foreach (char testing in child.transform.name)
             {
                 int tester = 0;
@@ -95,7 +97,7 @@ public class liveGameSetup : MonoBehaviour {
                 {
                     tester = int.Parse(testing + "");
                     newNum += testing;
-                    Debug.Log(testing);
+                    //Debug.Log(testing);
 
                     nameNum = int.Parse(newNum);
                     if (nameNum > highestNumericValue)
@@ -133,18 +135,75 @@ public class liveGameSetup : MonoBehaviour {
         movePoints = new levelProgression[highestNumericValue + 1];
         highestNumericValue = 0;
 
-        //int i = 0;
-        for(int i = 0; i < highestNumericValue; i++)
+        //Filter in all Transforms
+        transformOrder = 0;
         foreach (Transform child in children)
         {
+            int nameNum = 0;
+            int returning;
+            bool returnable;
 
-            int nameNum = int.Parse(child.transform.name);
-            if(nameNum == i)
+            string newNum = "";
+            //Debug.LogError("Require research and assistance converting transform name to int number!");
+            foreach (char testing in child.transform.name)
             {
-                children[highestNumericValue] = child;
+                int tester = 0;
+                //if(testing != int)
+                if (char.IsNumber(testing))
+                {
+                    tester = int.Parse(testing + "");
+                    newNum += testing;
+                    //Debug.Log(testing);
+
+                    nameNum = int.Parse(newNum);
+                    //if(returnTrueTransformOrder("hi"))
+                    //{
+                    //    Debug.LogWarning("Hello!");
+                    //}
+                    //Debug.LogError(transformOrder + " : " + tester);
+                    if (tester == transformOrder)
+                    {
+                        highestNumericValue = nameNum;
+                        movePoints[transformOrder].moveTo = child;
+                        movePoints[transformOrder].toProcess = child.GetComponentInChildren<progressionReportSystem>();
+                        if(movePoints[transformOrder].toProcess == null)
+                        {
+                            if(movePoints[transformOrder].moveTo.name.Contains("[stop]"))
+                            {
+                                movePoints[transformOrder].moveTo.name.Replace("[stop]", "");
+                            }
+                        }
+
+                        transformOrder++;
+                    }
+                }
             }
+
+
+            ////////////nameNum = int.Parse(child.transform.name);
+            ////////////if (nameNum == i)
+            ////////////{
+            ////////////    children[highestNumericValue] = child;
+            ////////////}
         }
     }
+
+    int transformOrder = 0;
+    /// <summary>
+    /// Returns a value if the string sent is a number based on filtering values.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    //bool returnTrueTransformOrder(string name)
+    //{
+    //    //get the string name. Process and check if it is a number. Check if it is == to the transform order number.
+    //    if(name != "hi")
+    //    {
+    //        return false;
+    //    }
+    //    else
+    //        return true;
+    //}
 
     void filterRecursion()
     {
