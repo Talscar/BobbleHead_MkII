ac67fb5c460e91570bb2fc194690d66ff6c8e6f9
Talscar-PC\Talscar <talscartalcom@gmail.com>
2018-01-26 01:27:10 +1030

    #Data serialization for saving and loading data for leaderboards and profiles.
    #Pre applying Priscilla Model update.

--- Assets/menuManager.cs
+++ Assets/menuManager.cs
@@ -8,68 +8,133 @@ public class menuManager : MonoBehaviour {
     public struct pageData
     {
         [SerializeField] public string pageName;
-        [SerializeField] GameObject pageObject;
+        [SerializeField] public GameObject pageObject;
         [SerializeField] public pageData[] pages;
     }
-    public pageData pages;
-    public int[] pageProgress;
+    public pageData[] pages;
+    [SerializeField] private int[] pageProgress;// = { 0 };
 
 
     //Depth
     //Length
 
-        //1, 2, 3, Depth
-        //2
-        //3
-        //Length
-        //
-        /*
-        newData = pageProgress;
-        [] = {0, 1, 2, 3, Depth}
-              ^  ^  ^  ^  ^
-              0      
-              1
-              2
-              3
-              Length
-    */
-        //
-        [SerializeField]public void basicMenuFunction(GameObject ActivateMenu, GameObject DissableMenu)
-    {
-        if(ActivateMenu != null)
-        ActivateMenu.SetActive(true);
-        if(DissableMenu != null)
-        DissableMenu.SetActive(false);
+    //1, 2, 3, Depth
+    //2
+    //3
+    //Length
+    //
+    /*
+    newData = pageProgress;
+    [] = {0, 1, 2, 3, Depth}
+          ^  ^  ^  ^  ^
+          0      
+          1
+          2
+          3
+          Length
+*/
+    //
+    //    [SerializeField]public void basicMenuFunction(GameObject ActivateMenu, GameObject DissableMenu)
+    //{
+    //    if(ActivateMenu != null)
+    //    ActivateMenu.SetActive(true);
+    //    if (DissableMenu != null)
+    //        DissableMenu.SetActive(false);
 
+    //}
+    //pages[0].pages[1].pages[3]
+    pageData recursedPage(int[] inputData)
+    {
+        ////////////////////////////////////////////////pageData[] newSelections = pages.pages;
+        ////////////////////////////////////////////////pageData newPage;// = recursedPage(inputData);
+        pageData[] newSelections = pages;
+        //pageData newPage;// = recursedPage(inputData);
+
+        //foreach depth in this thing
+        //get the new depth. Find the new location.
+        //return until the depth is met at that length
+
+        //Depth is how many things within a thing
+        //Length is how many things in an array
+        for (int i = 0; i < inputData.Length; i++)
+        {
+            //Debug.Log();
+            newSelections = newSelections[inputData[i]].pages;
+            //newPage = pages[i];
+        }
+        Debug.Log("NewSelections DATA: - " + newSelections.Length);
+ 
+        Debug.Log("InputData.Length beach = " + inputData.Length);
+        return newSelections[inputData[0]];//new pageData();
     }
 
     public void progressToPage(int nextPage)
     {
-        int[] newData = new int[pageProgress.Length + 1];
-
-        Debug.Log("Deactivate Old Page");
+        ///METHOD: recursedPage
+        //pageProgress[nextPage - 1].p
+        int[] newData;// = new int[pageProgress.Length];
+        if (pageProgress != null)
+        {
+            newData = new int[pageProgress.Length];
+            newData[pageProgress.Length - 1] = nextPage;
+        }
+        else
+        {
+            newData = new int[1];
+            newData[0] = nextPage;
+            pageProgress = newData;
+        }  
+        pageData thisData = recursedPage(newData);
+        thisData.pageObject.SetActive(false);
+        Debug.Log("Deactivate Old Page - " + thisData.pageObject.name);
+        newData = new int[pageProgress.Length + 1];
+        thisData = recursedPage(newData);
+        //newData = new int[pageProgress.Length + 1];
+        //thisData = recursedPage(newData);
         //int[] newData = new int[pageProgress.Length];
         for (int i = 0; i < pageProgress.Length; i++)
         {
             newData[i] = pageProgress[i];
         }
-        newData[newData.Length] = nextPage;
+        newData[newData.Length - 1] = nextPage;
         pageProgress = newData;
 
-
+        thisData = recursedPage(pageProgress);
+        thisData.pageObject.SetActive(true);
         Debug.Log("activate new page");
         //To get to the next page, we need to travese the pages tree using the int values to access the page we desire.
     }
 
     public void returnToPreviousPage()
     {
-        //pages[pag]
-        int[] newData = new int[pageProgress.Length - 1];
+        ///METHOD: recursedPage
+        //pageProgress[nextPage - 1].p
+        int[] newData = new int[pageProgress.Length];
+        pageData thisData = recursedPage(newData);
+        thisData.pageObject.SetActive(false);
+        Debug.Log("Deactivate Old Page - " + thisData.pageObject.name);
+        newData = new int[pageProgress.Length - 1];
+        thisData = recursedPage(newData);
+        //newData = new int[pageProgress.Length + 1];
+        //thisData = recursedPage(newData);
+        //int[] newData = new int[pageProgress.Length];
         for (int i = 0; i < newData.Length; i++)
         {
             newData[i] = pageProgress[i];
         }
+        //newData[newData.Length - 1] = nextPage;
         pageProgress = newData;
+
+        thisData = recursedPage(pageProgress);
+        thisData.pageObject.SetActive(true);
+        Debug.Log("activate new page");
+        //////////pages[pag]
+        ////////int[] newData = new int[pageProgress.Length - 1];
+        ////////for (int i = 0; i < newData.Length; i++)
+        ////////{
+        ////////    newData[i] = pageProgress[i];
+        ////////}
+        ////////pageProgress = newData;
     }
     public void closeMenu()
     {
